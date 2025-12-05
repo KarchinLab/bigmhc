62d1b1f2be429660dd53fe9c89e33a422c899909
Susanna Kiwala <susanna.kiwala@wustl.edu>
2025-12-05 10:44:46 -0600

    Use os.sched_getaffinity to get number of threads when running on a scheduler

--- src/cli.py
+++ src/cli.py
@@ -143,6 +143,13 @@ def _parseOut(args : argparse.Namespace) -> argparse.Namespace:
 
 def _parseJobs(args : argparse.Namespace) -> argparse.Namespace:
     if args.jobs is None or args.jobs <= 0:
+        if hasattr(os, 'sched_getaffinity'):
+            print("Using os.sched_getaffinity")
+            try:
+                args.jobs = len(os.sched_getaffinity(0))
+                return args
+            except Exception:
+                pass
         args.jobs = max(1, psutil.cpu_count(logical=False) // 2)
     return args
 
